77bc1cb6f944c020d2bb8be45bd5f44e479311d5
Ngô Chung Á Âu <>
2022-09-16 10:55:18 +0700

    upload file

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -15,6 +15,26 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
+const multer = require("multer");
+
+const upload = multer({
+  dest: "images",
+  limits: {
+    fileSize: 500000,
+  },
+  fileFilter(req, file, callback) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return callback(new Error("Must be image file"));
+    }
+
+    callback(undefined, true);
+  },
+});
+
+app.post("/upload", upload.single("file"), (req, res) => {
+  res.send(200);
+});
+
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
